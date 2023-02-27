0a8cb5540fa46d40e5093381a2b3036aadc5603a
Игорь <211353@mail.ru>
2023-02-27 22:43:18 +0300

    доделал задание с правильным выводом массивов

--- Program.cs
+++ Program.cs
@@ -1,46 +1,88 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int arrayCount ( String[] array){
+int arrayCount(String[] array)
+{
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i].Length <= 3) {
+        if (array[i].Length <= 3)
+        {
             count++;
         }
+    }
+    if (count == 0)
+    {
+        count = 1;
     }
     return count;
 }
 
-String[] newArray(String[] array,int count){
-    String[] arrayTreeSize = new String [count];
+String[] newArray(String[] array, int count)
+{
+    String[] arrayTreeSize = new String[count];
     int j = 0;
     for (int i = 0; i < array.Length; i++)
     {
-          if(array[i].Length <= 3) {
+        if (array[i].Length <= 3)
+        {
             arrayTreeSize[j] = array[i];
             j++;
         }
     }
+
+    if (j == 0)
+    {
+        arrayTreeSize[j] = "0000";
+    }
     return arrayTreeSize;
 }
-void printArray(String []array){
+void printArray(String[] array)
+{
     char kavuchki = '"';
-    Console.Write($"[{kavuchki}{array[0]}{kavuchki}");
-    for (int i = 1; i < array.Length - 1; i++)
+    if (array[0].Equals("0000"))
     {
-      Console.Write($" {kavuchki}{array[i]}{kavuchki}");  
+        Console.Write("[]");
     }
-    Console.Write($"{kavuchki}{array[array.Length - 1]}{kavuchki}]");
-} 
+    else if (array.Length >= 2)
+    {
 
-String [] firstArray = new String[] {"hello", "2", "world", ":-)"};
-int arraySize = arrayCount(firstArray);
+        Console.Write($"[{kavuchki}{array[0]}{kavuchki} ");
+
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            Console.Write($"{kavuchki}{array[i]}{kavuchki} ");
+        }
+        Console.Write($"{kavuchki}{array[array.Length - 1]}{kavuchki}]");
+    }
+    else
+    {
+        Console.Write($"[{kavuchki}{array[0]}{kavuchki}]");
+    }
+    // if(array.Length > 0){
+    // Console.Write($"{kavuchki}{array[array.Length - 1]}{kavuchki}]");
+
+
+    // }
+}
+
+
+String[] firstArray = new String[] { "hello", "2", "world", ":-)" };
+int arrayFirstSize = arrayCount(firstArray);
 printArray(firstArray);
 Console.Write(" -> ");
-printArray(newArray(firstArray, arraySize));
+printArray(newArray(firstArray, arrayFirstSize));
+
 Console.WriteLine();
-String [] thirdArray = new String[] {"hello", "2", "world", ":-)"};
+String[] secondArray = new String[] { "1234", "1567", "-2", "computer science" };
+int arraySecondSize = arrayCount(secondArray);
+printArray(secondArray);
+Console.Write(" -> ");
+printArray(newArray(secondArray, arraySecondSize));
+
+Console.WriteLine();
+String[] thirdArray = new String[] { "Russia", "Denmark", "Kazan" };
+int arrayThirdSize = arrayCount(thirdArray);
 printArray(thirdArray);
 Console.Write(" -> ");
-printArray(newArray(thirdArray, arraySize));
+printArray(newArray(thirdArray, arrayThirdSize));
